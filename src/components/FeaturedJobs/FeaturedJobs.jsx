54dbb50644227e92d213d36731326b60ec2b9932
jasaiful <jasaiful@gmail.com>
2023-09-23 17:31:07 +0600

    added card section

---
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import Job from "../Job/Job";
+
+
+const FeaturedJobs = () => {
+
+    const [jobs, setJobs] = useState([]);
+    // this is not the best way to load show all data
+    const [dataLength, setDataLength] = useState(4);
+
+    useEffect(() => {
+        fetch('jobs.json')
+            .then(res => res.json())
+            .then(data => setJobs(data));
+    }, [])
+
+    return (
+        <div>
+            <div>
+                <h2 className="text-4xl text-center mb-3">Featured Jobs: {jobs.length}</h2>
+                <p className="text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+
+            <div className="grid grid-cols-2 gap-6">
+                {
+                    jobs.slice(0, dataLength).map(job => <Job
+                        key={jobs.id}
+                        job={job}
+                    ></Job>)
+                }
+            </div>
+
+            <div className= "text-center @{dataLength === jobs.length && 'hidden'}">
+                <button
+                onClick={() => setDataLength(jobs.length)}
+                className="btn btn-primary bg-gradient-to-r from-red-500 to-white-500 hover:from-pink-500 hover:to-yellow-500 ...">Show All Jobs</button>
+            </div>
+
+        </div>
+    );
+};
+
+export default FeaturedJobs;
