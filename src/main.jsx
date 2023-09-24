08a843666ca7e48dcd45ee457a22ca3fd0df941e
jasaiful <jasaiful@gmail.com>
2023-09-24 17:23:06 +0600

    problem solved

--- src/main.jsx
+++ src/main.jsx
@@ -24,12 +24,12 @@ const router = createBrowserRouter([
       {
         path: "/applied",
         element: <AppliedJobs></AppliedJobs>,
-        loader: () => fetch ('../public/jobs.json') //only load the data you need. do not load all the data
+        loader: () => fetch ('/jobs.json') //only load the data you need. do not load all the data
       },
       {
         path: '/job/:id',
         element: <JobDetails></JobDetails>,
-        loader: () => fetch('../public/jobs.json') //do not load all data. load only what you need
+        loader: () => fetch('/jobs.json') //do not load all data. load only what you need
       }
     ]
   },
